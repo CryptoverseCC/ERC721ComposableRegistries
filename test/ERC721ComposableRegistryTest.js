5e822650495665be77cc6caf81b6bcb155ba6cca
ghostbuster91 <kghost0@gmail.com>
2018-04-25 22:34:06 +0200

    Fix assetion and restore untested code

--- test/ERC721ComposableRegistryTest.js
+++ test/ERC721ComposableRegistryTest.js
@@ -91,7 +91,7 @@ contract('ERC721ComposableRegistry', (accounts) => {
 	    erc721.approve(instance.address, 2);
         await instance.transfer(erc721.address, 1, erc721.address, 2);
         await instance.transferToAddress(accounts[0], erc721.address, 2);
-        const owner = await instance.ownerOf(erc721.address, 2);
+        const owner = await erc721.ownerOf(2);
 		assert.equal(owner, accounts[0]);
 	});
 });
