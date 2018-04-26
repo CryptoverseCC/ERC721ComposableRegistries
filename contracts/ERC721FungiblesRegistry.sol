75d67afbff908530486ae96e5605ba2694998928
Maciej Górski <maciek.gorski@gmail.com>
2018-04-26 16:00:17 +0200

    My token cannot transfer more than it owns.

--- contracts/ERC721FungiblesRegistry.sol
+++ contracts/ERC721FungiblesRegistry.sol
@@ -15,6 +15,7 @@ contract ERC721FungiblesRegistry {
     }
 
     function transferFrom(ERC721 fromErc721, uint fromTokenId, ERC721 toErc721, uint toTokenId, ERC20 erc20, uint amount) public {
+        require(balanceOf(fromErc721, fromTokenId, erc20) >= amount);
         balances[fromErc721][fromTokenId][erc20] -= amount;
         balances[toErc721][toTokenId][erc20] += amount;
     }
--- test/ERC721FungiblesRegistryTest.js
+++ test/ERC721FungiblesRegistryTest.js
@@ -84,3 +84,21 @@ contract('ERC721FungiblesRegistry', (accounts) => {
         assert.equal(balance, 30);
     });
 });
+
+contract('ERC721FungiblesRegistry', (accounts) => {
+
+    it("My token cannot transfer more than it owns", async () => {
+        const registry = await ERC721FungiblesRegistry.deployed();
+        const erc20 = await SampleERC20.deployed();
+        const erc721 = await SampleERC721.deployed();
+        await erc721.create();
+        await erc721.create();
+        await registry.transfer(erc721.address, 1, erc20.address, 50);
+        try {
+            await registry.transferFrom(erc721.address, 1, erc721.address, 2, erc20.address, 51);
+            assert.fail();
+        } catch (ignore) {
+            if (ignore.name === 'AssertionError') throw ignore;
+        }
+    });
+});
