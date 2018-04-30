9a9ae31831f2561643510f4142f5293f102e21d8
Maciej Górski <maciek.gorski@gmail.com>
2018-04-30 12:50:50 +0200

    [refactoring] Rename mappings.

--- contracts/ERC721ComposableRegistry.sol
+++ contracts/ERC721ComposableRegistry.sol
@@ -8,9 +8,9 @@ contract ERC721 {
 
 contract ERC721ComposableRegistry {
 
-    mapping (address => mapping (uint => TokenIdentifier)) parents;
+    mapping (address => mapping (uint => TokenIdentifier)) childToParent;
     mapping (address => mapping (uint => TokenIdentifier[])) parentToChildren;
-    mapping (address => mapping (uint => uint)) childIndexInParentToChildren;
+    mapping (address => mapping (uint => uint)) childToIndexInParentToChildren;
 
     struct TokenIdentifier {
         ERC721 erc721;
@@ -25,9 +25,9 @@ contract ERC721ComposableRegistry {
         uint toTokenId = bytesToUint(to, 32);
         require(ownerOf(toErc721, toTokenId) != 0);
         requireNoCircularDependency(toErc721, toTokenId, whichErc721, whichTokenId);
-        parents[whichErc721][whichTokenId] = TokenIdentifier(toErc721, toTokenId);
+        childToParent[whichErc721][whichTokenId] = TokenIdentifier(toErc721, toTokenId);
         parentToChildren[toErc721][toTokenId].push(TokenIdentifier(whichErc721, whichTokenId));
-        childIndexInParentToChildren[whichErc721][whichTokenId] = parentToChildren[toErc721][toTokenId].length - 1;
+        childToIndexInParentToChildren[whichErc721][whichTokenId] = parentToChildren[toErc721][toTokenId].length - 1;
         return 0xf0b9e5ba;
     }
 
@@ -46,11 +46,11 @@ contract ERC721ComposableRegistry {
         requireNoCircularDependency(toErc721, toTokenId, whichErc721, whichTokenId);
         address ownerOfWhichByErc721 = whichErc721.ownerOf(whichTokenId);
         whichErc721.transferFrom(ownerOfWhichByErc721, address(this), whichTokenId);
-        TokenIdentifier memory parent = parents[whichErc721][whichTokenId];
-        parents[whichErc721][whichTokenId] = TokenIdentifier(toErc721, toTokenId);
+        TokenIdentifier memory parent = childToParent[whichErc721][whichTokenId];
+        childToParent[whichErc721][whichTokenId] = TokenIdentifier(toErc721, toTokenId);
         if (parent.erc721 != ERC721(0)) {
             TokenIdentifier[] storage c = parentToChildren[parent.erc721][parent.tokenId];
-            uint index = childIndexInParentToChildren[whichErc721][whichTokenId];
+            uint index = childToIndexInParentToChildren[whichErc721][whichTokenId];
             uint last = c.length - 1;
             if (index < last) {
                 c[index] = c[last];
@@ -58,13 +58,13 @@ contract ERC721ComposableRegistry {
             c.length--;
         }
         parentToChildren[toErc721][toTokenId].push(TokenIdentifier(whichErc721, whichTokenId));
-        childIndexInParentToChildren[whichErc721][whichTokenId] = parentToChildren[toErc721][toTokenId].length - 1;
+        childToIndexInParentToChildren[whichErc721][whichTokenId] = parentToChildren[toErc721][toTokenId].length - 1;
     }
 
     function requireNoCircularDependency(ERC721 toErc721, uint toTokenId, ERC721 whichErc721, uint whichTokenId) private view {
         do {
             require(toErc721 != whichErc721 || toTokenId != whichTokenId);
-            TokenIdentifier memory parent = parents[toErc721][toTokenId];
+            TokenIdentifier memory parent = childToParent[toErc721][toTokenId];
             toErc721 = parent.erc721;
             toTokenId = parent.tokenId;
         } while (toErc721 != ERC721(0));
@@ -74,11 +74,11 @@ contract ERC721ComposableRegistry {
         require(ownerOf(whichErc721, whichTokenId) == msg.sender);
         address ownerOfWhichByErc721 = whichErc721.ownerOf(whichTokenId);
         whichErc721.transferFrom(ownerOfWhichByErc721, to, whichTokenId);
-        TokenIdentifier memory parent = parents[whichErc721][whichTokenId];
-        delete parents[whichErc721][whichTokenId];
+        TokenIdentifier memory parent = childToParent[whichErc721][whichTokenId];
+        delete childToParent[whichErc721][whichTokenId];
         if (parent.erc721 != ERC721(0)) {
             TokenIdentifier[] storage c = parentToChildren[parent.erc721][parent.tokenId];
-            uint index = childIndexInParentToChildren[whichErc721][whichTokenId];
+            uint index = childToIndexInParentToChildren[whichErc721][whichTokenId];
             uint last = c.length - 1;
             if (index < last) {
                 c[index] = c[last];
@@ -88,11 +88,11 @@ contract ERC721ComposableRegistry {
     }
 
     function ownerOf(ERC721 erc721, uint tokenId) public view returns (address) {
-        TokenIdentifier memory parent = parents[erc721][tokenId];
+        TokenIdentifier memory parent = childToParent[erc721][tokenId];
         while (parent.erc721 != ERC721(0)) {
             erc721 = parent.erc721;
             tokenId = parent.tokenId;
-            parent = parents[erc721][tokenId];
+            parent = childToParent[erc721][tokenId];
         }
         return erc721.ownerOf(tokenId);
     }
